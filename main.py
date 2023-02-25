8e4fe0fcb1f5700f352671c841ee10606b825867
Кирюшин Виталий <kiryuschin.vitalij@yandex.ru>
2023-02-25 19:19:51 +0300

    Add first commit

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import cv2
 from sklearn.svm import LinearSVC
+
+
+def extract_histogram(image, bins=(8, 8, 8)):
+    hist = cv2.calcHist([image], [0, 1, 2], None, bins, [0, 256, 0, 256, 0, 256])
+    cv2.normalize(hist, hist)
+    return hist.flatten()
+
+
+if __name__ == '__main__' :
+    cmd = None
+
 
